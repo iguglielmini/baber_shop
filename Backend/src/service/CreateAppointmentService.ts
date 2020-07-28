f05adc2cf9ea58e288592555181978cb2b751094
Italo Guglielmini <italo.guglielmini@gmail.com>
2020-07-28 10:21:22 -0300

    Criaçaão do backend do Baber Shop em TS, criando os models, services, routes e repository de agendamento

---
+++ Backend/src/service/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import Appointment from '../models/Appointment'
+import { startOfHour } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+
+interface Request {
+    provider: string;
+    date: Date;
+}
+
+
+class CreateAppointmentService {
+    private appointmentsRepository: AppointmentsRepository;
+
+    constructor(appointmentsRepository: AppointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+    }
+
+    public execute({ date, provider }: Request): Appointment {
+        const appointmentDate = startOfHour(date);
+
+        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (findAppointmentInSameDate) {
+            throw Error('This is appointment is already booked');
+        }
+
+        const appointment = this.appointmentsRepository.create({
+            provider,
+            date: appointmentDate,
+        });
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
